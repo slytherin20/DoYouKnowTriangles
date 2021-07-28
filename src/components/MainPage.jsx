725c17af7515672f24547350afda78eea487071a
slytherin20 <sonaligupta2904@gmail.com>
2021-07-28 19:58:19 +0530

    Style - Stylesheet updated

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -3,6 +3,19 @@ import { Link } from "react-router-dom";
 function MainPage(){
 return(
     <>
+    <div className="background-anim">
+        <div className="triangle-1">
+            <div className="triangle-1-white">
+            <div className="triangle-2">
+                <div className="triangle-2-white">
+                    <div className="triangle-3">
+                        <div className="triangle-3-white"></div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
     <div className="description">
     <h1 className="heading">Do You Know Triangles?</h1>
     <p>A series of fun games to check your knowlege on triangles.</p>
